9bd1ccf0fd2429cc954726333011e289b1d8080d
Jose Garcia <jogarcia@tuta.io>
2021-03-17 20:48:20 -0400

    Check that a function repeated over a timeout is bien called once

--- tests/Debouncer.spec.js
+++ tests/Debouncer.spec.js
@@ -8,4 +8,17 @@ describe("debounce", function () {
   it("Should exist", function () {
     expect(debounce).to.exist;
   });
+
+  it("Should execute the function only once", function () {
+    let i = 0;
+    const callbackFunction = () => {
+      i++;
+    };
+    for (let index = 5; index > 0; index--) {
+      debounce(() => {
+        callbackFunction();
+        expect(i).to.equal(1);
+      }, 10);
+    }
+  });
 });
